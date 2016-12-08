9ff300076956142d7207b3c3430ec70b220c9573
Ward Weistra <ward@thehyve.nl>
2016-12-07 21:58:49 -0500

    Make query be processed on backend

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('generator.html')
+	return render_template('generator.html')
+
+@app.route(('/json_to_query/'), methods=['POST'])
+def json_to_query():
+	feedback = json.dumps(request.get_json())
+	return json.jsonify(feedback=feedback)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
