fc90766c5172067b71effe480b7718a16d2e4196
Ward Weistra <ward@thehyve.nl>
2016-12-10 19:58:37 -0500

    Converting visual query to tranSMART query on backend. Included form for entity and server url setting.

--- generator/__init__.py
+++ generator/__init__.py
@@ -5,10 +5,52 @@ app = Flask(__name__)
 def hello():
 	return render_template('generator.html')
 
+def convert_to_transmart(genericquery):
+
+		transmartquery = []
+
+		for genericquerypart in genericquery:
+			subtransmartquery = {}
+			condition = genericquerypart.get('condition')
+
+			if condition is not None:
+				subgenericquery = genericquerypart['rules']
+				childtransmartquery, lengthchildren = convert_to_transmart(subgenericquery)
+				
+				if lengthchildren == 1:
+					subtransmartquery = childtransmartquery[0]
+				else:
+					subtransmartquery['type'] = "Combination"
+					subtransmartquery['operator'] = condition.lower()
+					subtransmartquery['args'] = childtransmartquery
+
+				if genericquerypart.get('not') == True:
+					notsubtransmartquery = {}
+					notsubtransmartquery['type'] = 'Negation'
+					notsubtransmartquery['arg'] = subtransmartquery
+					subtransmartquery = notsubtransmartquery
+
+			elif genericquerypart['field'] == 'StudyNameConstraint':
+				subtransmartquery['type'] = 'StudyNameConstraint'
+				subtransmartquery['studyId'] = genericquerypart['value']
+
+			elif genericquerypart['field'] == 'ConceptConstraint':
+				subtransmartquery['type'] = 'ConceptConstraint'
+				subtransmartquery['path'] = genericquerypart['value']
+
+			transmartquery += [subtransmartquery]
+		
+		return transmartquery, len(genericquery)
+
 @app.route(('/json_to_query/'), methods=['POST'])
 def json_to_query():
-	feedback = json.dumps(request.get_json())
-	return json.jsonify(feedback=feedback)
+	querydata = request.get_json()
+	genericquery = querydata['genericquery']
+
+	transmartquery, lengthchildren = convert_to_transmart([genericquery])
+
+	transmartquery = genericquery = querydata['url'] + '/v2/' + querydata['entities'] + '?constraint=' + json.dumps(transmartquery[0])
+	return json.jsonify(transmartquery=transmartquery)
 
 if __name__ == "__main__":
 	app.run()
